d4883fefce3ddd5ba7e9b9baa4c5e0be642fd9d0
RonitGajbhiye358 <gajbhiyeronit2003@gmail.com>
2024-07-22 23:13:45 +0530

    final commit

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -26,7 +26,7 @@ function LogoutBtn() {
           alt="Logout"
           className="invert w-7 h-7 group-hover:invert-0"
         />
-        <div className="collapse md:visible w-0 md:w-auto group-hover:invert-0">Sign Out</div>
+        <div className="collapse md:visible w-0 md:w-auto group-hover:invert">Sign Out</div>
       </button>
     </div>
   );
