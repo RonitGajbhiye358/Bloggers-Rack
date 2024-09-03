eb6893ccf9dca0f2249ed83345edc05b2393dc1d
Ronit Gajbhiye <154435117+RonitGajbhiye358@users.noreply.github.com>
2024-09-03 15:51:20 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,12 +1,12 @@
 const conf = {
-  appwriteUrl: "https://cloud.appwrite.io/v1",
-  appwriteProjectId: "66921aca002b331a1b98",
-  appwriteDatabaseId: "6692200100072be0ae18",
-  appwriteCollectionId: "66922023002a0ad074f4",
-  appwriteBucketId: "669220f3002b31311ed5",
-  rteapi:"7wvp6wqz7fek7yej0t2u7xxen9t782grp47mth9278m5rq0n",
-  appwriteFriendRequestsCollectionId:"66d00057000b18f801a7",
-  appwriteFriendsCollectionId:"66d004e60001950d3e01"
+  appwriteUrl: " ",
+  appwriteProjectId: " ",
+  appwriteDatabaseId: " ",
+  appwriteCollectionId: " ",
+  appwriteBucketId: " ",
+  rteapi:" ",
+  appwriteFriendRequestsCollectionId:" ",
+  appwriteFriendsCollectionId:" "
 }
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
